4be8578c360f45449d7b4c6a6a9a241c1a4a0fc5
Ahmed Nagra <ahmednagra244@gmail.com>
2025-12-31 12:32:54 +0500

    Some fixes and changes

--- app/(pages)/(auth)/verify/page.tsx
+++ app/(pages)/(auth)/verify/page.tsx
@@ -1,77 +1,13 @@
+// app/auth/verify/page.tsx
 "use client";
 
-import { VerifyUserInput, verifyUserSchema } from '@/schemas/verifyUserSchema';
-import { zodResolver } from '@hookform/resolvers/zod';
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { toast } from 'sonner';
-import { useRouter } from "next/navigation";
-import { useAppContext } from '@/context/AppContext';
+import React, { Suspense } from "react";
+import VerificationForm from "@/components/VerificationForm";
 
-const VerificationForm = () => {
-    const router = useRouter();
-
-    const [isLoading, setIsLoading] = useState(false);
-
-    const { register, handleSubmit, formState } = useForm<VerifyUserInput>({ resolver: zodResolver(verifyUserSchema) });
-    const { errors } = formState;
-    const onSubmit = async (data: VerifyUserInput) => {
-        const { user } = useAppContext();
-        const email = user?.email;
-
-        if (!email) {
-            toast.error("No email found, please try again");
-            return;
-        }
-
-        try {
-            setIsLoading(true);
-
-            const res = await fetch("/api/auth/verify", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    ...data,
-                    email
-                }),
-
-            })
-            const result = await res.json();
-
-            if (!res.ok) {
-                throw new Error(result.message || "Something went wrong");
-            }
-
-            router.push("/signin")
-            toast.success("Account verified successfully");
-
-        } catch (error) {
-            if (error instanceof Error) {
-                toast.error(error.message);
-            } else {
-                toast.error("Unexpected error occured");
-
-            }
-        } finally {
-            setIsLoading(false);
-        }
-    }
-
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <input type="text" placeholder="Verification Code" {...register("verificationCode")} />
-                {errors.verificationCode && <p>{errors.verificationCode.message}</p>}
-            </div>
-            <div>
-                <button type="submit" disabled={isLoading}>
-                    {isLoading ? "Verifying account..." : "Verify"}
-                </button>
-            </div>
-        </form>
-    )
+export default function VerifyPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <VerificationForm />
+    </Suspense>
+  );
 }
-
-export default VerificationForm
